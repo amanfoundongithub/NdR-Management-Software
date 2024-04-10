a46e996cf93f02fdf943b411d616a4d3521a40a0
Mohammad Zaid <88818297+zetapii@users.noreply.github.com>
2024-04-10 15:01:58 +0530

    Delete NdR directory

--- NdR/AutomatedSystems/ParkingLots/Implementations/ParkingLotB.py
+++
@@ -1,8 +0,0 @@
-from ..Abstract.ParkingLot import ParkingLot
-
-class ParkingLotB(ParkingLot):
-    '''
-    Initializes a 150 passenger parking Lot 
-    '''
-    def __init__(self, userbus):
-        super().__init__("Parking Lot B",150, userbus)
