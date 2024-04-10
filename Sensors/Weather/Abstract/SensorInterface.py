a6cd89be90b9ef35f6ea813525e4762170903ca1
Mohammad Zaid <88818297+zetapii@users.noreply.github.com>
2024-04-10 15:02:35 +0530

    Delete Sensors directory

--- Sensors/Weather/Abstract/SensorInterface.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-
-class Sensor(ABC):
-    """
-    An interface for implementation of the sensors
-    """
-    @abstractmethod
-    def sendValues(self):
-        pass 
